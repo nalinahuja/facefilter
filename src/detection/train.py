6d54c5537784c1d86481629888bf4b0f0965c082
Nalin Ahuja <nalinahuja22@gmail.com>
2021-12-03 00:18:44 -0500

    Project Backup

--- src/detection/train.py
+++ src/detection/train.py
@@ -12,45 +12,36 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 SEED_VALUE = 1618
 
 # Print Format Strings
-NL, CR = "\n", "\r"
+NL, TB, CR = "\n", "\t", "\r"
 
 # TensorFlow Model Paths
 TF_DATA_PATH = "./data"
 TF_SAVE_PATH = "./saved"
 
 # Training Data Size
-TRAIN_SIZE = 0.75
+TRAIN_SIZE = 0.80
 
 # TensorFlow Model Hyperparameters
-TF_DROP_OUT = 0.20
+TF_DROP_OUT = 0.25
 TF_NUM_EPOCHS = 10
-TF_LEARNING_RATE = 0.001
-
-# Set Number Of Classes
-OUTPUT_SIZE = 2
+TF_BATCH_SIZE = 32
+TF_LEARNING_RATE = 0.01
 
 # Set Input Dimensions
-INPUT_Y = 60
-INPUT_X = 60
-INPUT_Z = 3
+INPUT_X = 96
+INPUT_Y = 96
+INPUT_Z = 1
 
-# Set Input Size
-INPUT_SIZE = INPUT_X * INPUT_Y * INPUT_Z
+# Evaluation Sample Size
+EVALUATION_SIZE = 5
 
 # End Embedded Constants------------------------------------------------------------------------------------------------------------------------------------------------
 
 import random
 import cv2 as cv
 import numpy as np
-import tensorflow as tf
 import pandas as pd
-import json
-import codecs
-from tqdm import tqdm
-import requests
-from PIL import Image
-import io
-from io import BytesIO
+import tensorflow as tf
 
 from sklearn import metrics
 from tensorflow import keras
@@ -66,39 +57,39 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 # End Module Imports----------------------------------------------------------------------------------------------------------------------------------------------------
 
-def build_tf_conv_net(x_train, y_train, eps = TF_NUM_EPOCHS, lr = TF_LEARNING_RATE, drop_out = True, drop_rate = TF_DROP_OUT):
+def build_tf_conv_net(x_train, y_train, x_test, y_test, eps = TF_NUM_EPOCHS, lr = TF_LEARNING_RATE, bs = TF_BATCH_SIZE, drop_out = True, drop_rate = TF_DROP_OUT):
     # Initialize New Sequential Model Instance
     model = keras.Sequential()
 
     # Add Convolutional Network Layers
-    model.add(keras.layers.Conv2D(32, kernel_size = [3, 3], activation = tf.nn.relu, input_shape = [INPUT_X, INPUT_Y, INPUT_Z]))
-    model.add(keras.layers.Conv2D(32, kernel_size = [3, 3], activation = tf.nn.relu))
+    model.add(keras.layers.Conv2D(32, kernel_size = [3, 3], activation = tf.nn.tanh, input_shape = [INPUT_X, INPUT_Y, INPUT_Z]))
+    model.add(keras.layers.Conv2D(32, kernel_size = [3, 3], activation = tf.nn.tanh))
 
     # Add Pooling And Normalization Layers
     model.add(keras.layers.MaxPooling2D(pool_size = [2, 2]))
     model.add(keras.layers.BatchNormalization())
 
     # Add Convolutional Network Layers
-    model.add(keras.layers.Conv2D(64, kernel_size = [3, 3], activation = tf.nn.relu))
-    model.add(keras.layers.Conv2D(64, kernel_size = [3, 3], activation = tf.nn.relu))
+    model.add(keras.layers.Conv2D(64, kernel_size = [3, 3], activation = tf.nn.tanh))
+    model.add(keras.layers.Conv2D(64, kernel_size = [3, 3], activation = tf.nn.tanh))
 
     # Add Pooling And Normalization Layers
     model.add(keras.layers.MaxPooling2D(pool_size = [2, 2]))
     model.add(keras.layers.BatchNormalization())
-
+    
     # Add Flattening Layer
     model.add(keras.layers.Flatten())
 
     # Add Dense Layer
-    model.add(keras.layers.Dense(256, activation = tf.nn.relu))
+    model.add(keras.layers.Dense(256, activation = tf.nn.tanh))
 
     # Check Dropout Setting
     if (drop_out):
         # Add Dropout Layer
         model.add(keras.layers.Dropout(drop_rate, input_shape = [2]))
-
+    
     # Add Dense Layer
-    model.add(keras.layers.Dense(128, activation = tf.nn.relu))
+    model.add(keras.layers.Dense(128, activation = tf.nn.tanh))
 
     # Check Dropout Setting
     if (drop_out):
@@ -106,10 +97,10 @@ def build_tf_conv_net(x_train, y_train, eps = TF_NUM_EPOCHS, lr = TF_LEARNING_RA
         model.add(keras.layers.Dropout(drop_rate, input_shape = [2]))
 
     # Add Output Layer
-    model.add(keras.layers.Dense(OUTPUT_SIZE, activation = tf.nn.softmax))
+    model.add(keras.layers.Dense(y_train.shape[1], activation = tf.nn.sigmoid))
 
     # Initialize Loss Function
-    loss_func = keras.losses.categorical_crossentropy
+    loss_func = keras.losses.mse
 
     # Initialize Model Optimizer
     opt_func = tf.optimizers.Adam(learning_rate = lr)
@@ -118,32 +109,15 @@ def build_tf_conv_net(x_train, y_train, eps = TF_NUM_EPOCHS, lr = TF_LEARNING_RA
     model.compile(loss = loss_func, optimizer = opt_func, metrics = ["accuracy"])
 
     # Train Model
-    model.fit(x_train, y_train, epochs = eps)
+    model.fit(x_train, y_train, epochs = eps, batch_size = bs, validation_data = (x_test, y_test), verbose = True)
 
     # Print Separator
-    print("NL" * 1, end = "")
+    print(NL * 1, end = "")
 
     # Return Model
     return (model)
 
 # End Classifier Functions----------------------------------------------------------------------------------------------------------------------------------------------
-
-def encode_preds(preds):
-    # Initialize Encoded Predictions Representation
-    enc = np.zeros(preds.shape)
-
-    # Determine Indicies With Maximum Probability
-    mpi = np.argmax(preds, axis = 1)
-
-    # Iterate Over Predictions
-    for i in range(preds.shape[0]):
-        # Set Position Of Maximum Probability
-        enc[i][mpi[i]] = 1
-
-    # Return Encoded Predictions Representation
-    return (enc)
-
-# End Utility Functions------------------------------------------------------------------------------------------------------------------------------------------------
 
 def get_data():
     # Check For Data Path
@@ -153,60 +127,56 @@ def get_data():
 
     # Display Status
     print(CR + "Loading dataset...", end = "")
-
-    # TODO, load data from TF_DATA_PATH
     
-    #x = type: ndarray
-    #y = type: ndarray
-    #https://www.kaggle.com/dataturks/face-detection-in-images
-    #x = pd.read_json("data/face_detection.json")
-    #f = open("data/face_detection.json")
-    #x = json.load(f)
-    fname = "data/face_detection.json"
-    jsonData = []
-    
-    with codecs.open(fname, 'rU') as js:
-        for line in js:
-            jsonData.append(json.loads(line))
-            
-    x = []
-    y = []
-    
-    print("json data 0 : ", jsonData[0])
+    # TODO: Load Dataset
 
-    i = 0
-    for data in tqdm(jsonData):
-        print("i : ", i)
-        i = i + 1
-        if i < 10:
-            continue
-        print("data content : ", data['content'])
-        response = requests.get(data['content'])
-        print("response : ", response.content)
-        byteImgIO = io.BytesIO()
-        byteImg = Image.open(response.content)
-        byteImg.save(byteImgIO, "PNG")
-        byteImgIO.seek(0)
-        byteImg = byteImgIO.read()
-        img = np.asarray(Image.open(BytesIO(byteImg)))
-        #images.append([img, data["annotation"]])
-        #images.append(img)
-        metadata = data["annotation"]
-        for data in metadata:
-            height = data['imageHeight']
-            width = data['imageWidth']
-            points = data['points']
-            if 'Face' in data['label']:
-                start_x = round(width*points[0]['x'])
-                start_y = round(height*points[0]['y'])
-                end_x = round(width*points[1]['x'])
-                end_y = round(height*points[1]['y'])
-                target = (start_x, start_y, end_x, end_y)
-                x.append(img)
-                y.append(np.asarray(target))
+    # # Extract Input Face Data
+    # face_images = np.moveaxis(np.load(os.path.join(TF_DATA_PATH, "face_images.npz"))["face_images"], -1, 0)
 
-    # Split Columnar Spam Data
-    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = TRAIN_SIZE)
+    # # Load Output Keypoint Data
+    # face_keypoints = pd.read_csv(os.path.join(TF_DATA_PATH, "facial_keypoints.csv")).fillna(0)
+
+    # # Select Face Keypoint Indexes
+    # face_indexes = np.where(
+    #     face_keypoints.nose_tip_x.notna() &
+    #     face_keypoints.left_eye_center_x.notna() &
+    #     face_keypoints.right_eye_center_x.notna() &
+    #     face_keypoints.mouth_center_bottom_lip_x.notna()
+    # )[0]
+
+    # # Get Image Side Dimension
+    # dim = face_images.shape[1]
+
+    # # Get Sample Count
+    # sc = face_indexes.shape[0]
+    
+    # # Initialize Input Data Vector
+    # x = np.zeros((sc, dim, dim, 1))
+    
+    # # Set Input Data Vector Values
+    # x[:, :, :, 0] = np.divide(face_images[face_indexes, :, :], 255)
+
+    # # Initialize Output Data Vector
+    # y = np.zeros((sc, 8))
+    
+    # # Set Nose Tip Keypoint Values
+    # y[:, 0] = np.divide(face_keypoints.nose_tip_x[face_indexes], dim)
+    # y[:, 1] = np.divide(face_keypoints.nose_tip_y[face_indexes], dim)
+    
+    # # Set Left Eye Center Keypoint Values
+    # y[:, 2] = np.divide(face_keypoints.left_eye_center_x[face_indexes], dim)
+    # y[:, 3] = np.divide(face_keypoints.left_eye_center_y[face_indexes], dim)
+
+    # # Set Right Eye Center Keypoint Values
+    # y[:, 4] = np.divide(face_keypoints.right_eye_center_x[face_indexes], dim)
+    # y[:, 5] = np.divide(face_keypoints.right_eye_center_y[face_indexes], dim)
+
+    # # Set Mouth Center Bottom Lip Keypoint Values
+    # y[:, 6] = np.divide(face_keypoints.mouth_center_bottom_lip_x[face_indexes], dim)
+    # y[:, 7] = np.divide(face_keypoints.mouth_center_bottom_lip_y[face_indexes], dim)
+
+    # Split Facial Detection Data
+    x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle = True, train_size = TRAIN_SIZE)
 
     # Convert Input Data Into Numpy Arrays
     x_train = np.array(x_train)
@@ -217,81 +187,34 @@ def get_data():
     y_test = np.array(y_test)
 
     # Display Information About Dataset
-    print("Dataset: %s" % DATASET)
-    print("Shape of x_train dataset: %s." % str(x_train.shape))
-    print("Shape of y_train dataset: %s." % str(y_train.shape))
-    print("Shape of x_test dataset: %s." % str(x_test.shape))
-    print("Shape of y_test dataset: %s." % str(y_test.shape))
-    print("NL" * 1, end = "")
+    print(CR + "Shape of x_train dataset: %s" % str(x_train.shape))
+    print("Shape of y_train dataset: %s" % str(y_train.shape))
+    print("Shape of x_test dataset: %s" % str(x_test.shape))
+    print("Shape of y_test dataset: %s" % str(y_test.shape))
+    print(NL * 1, end = "")
 
     # Return Data
     return ((x_train, y_train), (x_test, y_test))
 
-def process_data(raw):
-    # Unpack Data From Raw Input
-    ((x_train, y_train), (x_test, y_test)) = raw
-
-    # Process Training Input Images
-    for i, img in enumerate(x_train):
-        # Convert Image Data Type
-        img = img.astype("float64")
-
-        # Resize Training Input Image To Specified Dimensions
-        img = cv.resize(img, dsize = (INPUT_X, INPUT_Y))
-
-        # Add Dimension To Numpy Array
-        img = np.expand_dims(img, axis = 0)
-
-        # Update Training Data Point
-        x_train[i] = img
-
-    # Process Testing Input Images
-    for i, img in enumerate(x_test):
-        # Convert Image Data Type
-        img = img.astype("float64")
-
-        # Resize Testing Input Image To Specified Dimensions
-        img = cv.resize(img, dsize = (INPUT_X, INPUT_Y))
-
-        # Add Dimension To Numpy Array
-        img = np.expand_dims(img, axis = 0)
-
-        # Update Training Data Point
-        x_test[i] = img
-
-    # Process Integer Arrays Into Binary Class Matrices
-    y_train = keras.utils.to_categorical(y_train, OUTPUT_SIZE)
-    y_test = keras.utils.to_categorical(y_test, OUTPUT_SIZE)
-
-    # Display Information About Dataset
-    print("New shape of x_train dataset: %s." % str(x_train.shape))
-    print("New shape of x_test dataset: %s." % str(x_test.shape))
-    print("New shape of y_train dataset: %s." % str(y_train.shape))
-    print("New shape of y_test dataset: %s." % str(y_test.shape))
-    print("NL" * 1, end = "")
-
-    # Return Preprocessed Data
-    return ((x_train, y_train), (x_test, y_test))
-
-def train_model(data):
+def train_model(train_data, test_data):
     # Unpack Training Data
-    x_train, y_train = data
+    x_train, y_train = train_data
+    
+    # Unpack Testing Data
+    x_test, y_test = test_data
 
     # Display Status
     print("Training Tensorflow convolutional network..." + NL)
 
     # Return Model
-    return (build_tf_conv_net(x_train, y_train, eps = TF_NUM_EPOCHS))
+    return (build_tf_conv_net(x_train, y_train, x_test, y_test, eps = TF_NUM_EPOCHS))
 
 def run_model(data, model):
     # Display Status
-    print("Running Tensorflow convolutional network..." + NL)
-
+    print("Running Tensorflow convolutional network...")
+    
     # Run TensorFlow Convolutional Model On Data
     preds = model.predict(data)
-
-    # One Hot Encode Predictions
-    preds = encode_preds(preds)
 
     # Return Predictions
     return (np.array(preds))
@@ -300,64 +223,61 @@ def eval_results(data, y_pred):
     # Unpack Testing Data
     _, y_test = data
 
-    # Format Test Data
-    y_test = np.argmax(y_test, axis = 1)
+    # Randomly Sample Predicted Outputs
+    samples = random.sample(range(len(y_pred)), EVALUATION_SIZE)
 
-    # Format Prediction Data
-    y_pred = np.argmax(y_preds, axis = 1)
+    # Iterate Over Selected Predicted Outputs 
+    for i in (samples):
+        # Compute Sum Of Squared Output Differences
+        l2_error = np.sum(np.square(np.subtract(y_test[i], y_pred[i])))
 
-    # Compute Model Accuracy
-    acc = metrics.accuracy_score(y_test, y_pred)
+        # Round Test Output Vector
+        y_test[i] = np.round(y_test[i], decimals = 3)
 
-    # Display Model Accuracy
-    print(NL + "Classifier Accuracy: {:.3f}".format(acc))
+        # Round Predicted Output Vector
+        y_pred[i] = np.round(y_pred[i], decimals = 3)
 
-    # Generate Model Classification Report
-    cr = metrics.classification_report(y_test, y_pred, target_names = ["Positive", "Negative"])
+        # Print Sample Index
+        print(NL + "Sample Index %d:" % int(i))
 
-    # Display Model Classification Report
-    print(NL + "Classifier Report:")
-    print(cr)
+        # Print Expected Output
+        print(TB + "Test: " + str(y_test[i]))
+        
+        # Print Predicted Output
+        print(TB + "Pred: " + str(y_pred[i]))
 
-    # Generate Confusion Matrix
-    cm = metrics.confusion_matrix(y_test, y_pred)
-
-    # Display Confusion Matrix
-    print("Confusion Matrix:")
-    print(cm)
+        # Print L2 Error
+        print(TB + "L2 Error: %.2f" % float(l2_error))
 
 # End Pipeline Functions------------------------------------------------------------------------------------------------------------------------------------------------
 
-if (__name__ == '__main__'):
+if (__name__ == "__main__"):
     # Ignore Numpy Warnings
     np.seterr(all = "ignore")
 
-    # Get Raw Data
-    raw = get_data()
+    # Get Processed Data
+    data = get_data()
 
-    # Process Raw Data
-    data = process_data(raw)
+    # Train Model On Processed Data
+    model = train_model(data[0], data[1])
 
-    # Train Model On Raw Data
-    model = train_model(data[0])
-
-    # Run Model On Raw Data
+    # Run Model On Processed Data
     preds = run_model(data[1][0], model)
 
     # Evaluate Model Results
     eval_results(data[1], preds)
 
     # Check For Model File
-    if (os.path.exists(TF_MODEL_PATH)):
+    if (os.path.exists(TF_SAVE_PATH)):
         # Get Model Overwrite Confirmation
-        if (input(NL + "Model file exists, confirm overwrite [y/n]: ").lower() != "y"):
-            # Exit Program
-            sys.exit()
+        if (len(os.listdir(TF_SAVE_PATH)) and input(NL + "Model file exists, confirm overwrite [y/n]: ").lower() != "y"):
+            # Print Separator
+            print(NL * 1, end = "")
+        else:
+            # Print Status
+            print(NL + "Saving TensorFlow model to disk..." + NL)
 
-        # Print Status
-        print(NL + "Saving TensorFlow model to disk..." + NL)
-
-        # Save Model To Disk
-        model.save(TF_MODEL_PATH)
+            # Save Model To Disk
+            model.save(TF_SAVE_PATH)
 
 # End Main Function-----------------------------------------------------------------------------------------------------------------------------------------------------
--- src/main.py
+++ src/main.py
@@ -9,11 +9,17 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 # End Imports------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Print Format Strings
-NL, CR = "\n", "\r"
+NL, TB, CR = "\n", "\t", "\r"
 
 # TensorFlow Model Paths
 FACE_DETECTION_MODEL = "./detection"
 FACE_MAPPING_MODEL = "./mapping"
+
+# Video Capture Resolution
+VIDEO_CAPTURE_RESOLUTION = (1280, 720)
+
+# Image Mask Names
+MASKS = ["glasses.png", "nose.png", "tongue.png"]
 
 # End Embedded Constants-------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -30,23 +36,87 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 # End Module Imports-----------------------------------------------------------------------------------------------------------------------------------------------------
 
+# Print Status
+print("Loading facial detection model..." + NL)
+
 try:
     # Load Face Detection Model
-    FACE_DETECTOR = keras.models.load_model(os.path.join(FACE_DETECTION_MODEL, "saved"))
+    face_detector = keras.models.load_model(os.path.join(FACE_DETECTION_MODEL, "saved"))
 except Exception as e:
     # Raise FileNotFoundError
     raise FileNotFoundError("could not load facial detection model")
 
+def detect_face(image):
+    # TODO: Implement This Wrapper
+
+# End Facial Detection Model Loading-------------------------------------------------------------------------------------------------------------------------------------
+
+# Print Status
+print("Loading facial mapping model..." + NL)
+
 try:
     # Load Facial Mapping Model
-    FACE_MAPPER = keras.models.load_model(os.path.join(FACE_MAPPING_MODEL, "saved"))
+    face_mapper = keras.models.load_model(os.path.join(FACE_MAPPING_MODEL, "saved"))
 except Exception as e:
     # Raise FileNotFoundError
     raise FileNotFoundError("could not load facial mapping model")
 
-# Load Models------------------------------------------------------------------------------------------------------------------------------------------------------------
+def map_face(image):
+    # Convert Image Colorspace To Grayscale
+    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    # Normalize Image Pixel Values
+    image = np.divide(image, 255)
+
+    # Expand Pixel Value Dimension
+    image = np.expand_dims(image, axis = -1)
+
+    # Expand Image Frame Dimension
+    image = np.expand_dims(image, axis = 0)
+
+    # Run Image Through Facial Mapping Model
+    keypoints = face_mapper.predict(image)[0]
+
+    # Scale Keypoints To Fit Image Dimensions
+    keypoints = np.multiply(keypoints, image.shape[0])
+    
+    # Round Keypoint Coordinates  
+    keypoints = np.round(keypoints)
+
+    # Cast Keypoint Values
+    keypoints = keypoints.astype(np.int32)
+
+    # Initialize Feature Dictionary
+    features = dict()
+
+    # Set Nose Tip Coordinates
+    features["nose_tip"] = tuple(keypoints[0 : 2])
+
+    # Set Left Eye Coordinates
+    features["left_eye"] = tuple(keypoints[2 : 4])
+
+    # Set Right Eye Coordinates
+    features["right_eye"] = tuple(keypoints[4 : 6])
+
+    # Set Mouth Center Coodinates
+    features["mouth_center"] = tuple(keypoints[6 : 8])
+
+    # Return Featues Dictionary
+    return (features)
+
+# End Facial Mapping Model Loading---------------------------------------------------------------------------------------------------------------------------------------
+
+def create_mask():
+    pass
+
+def 
+
+# End Image Masking Functions--------------------------------------------------------------------------------------------------------------------------------------------
 
 if (__name__ == "__main__"):
+    # Print Status
+    print("Starting video capture..." + NL)
+
     # Create Video Stream
     stream = cv.VideoCapture(0)
 
@@ -55,15 +125,16 @@ if (__name__ == "__main__"):
 
     # Process Frames
     while (ret):
-        # Convert Grame Colorspace To Grayscale
-        gs = cv.cvtColor(fr, cv.COLOR_BGR2GRAY)
+        # Resize Video Frame To Fixed Dimensions
+        fr = cv.resize(fr, dsize = VIDEO_CAPTURE_RESOLUTION, interpolation = cv.INTER_AREA)
 
-        # TODO: Run Frame Through Facial Detector
-            # TODO: If bounding boxes exist, map each face in a bounding box
+        # TODO: Run Video Frame Through Facial Detector
 
-            # TODO: Convert frame to black and white
-            
-            # TODO: Add features to face on bounding box
+        # TODO: Verify Faces Exist In Frame
+
+            # TODO: Map Each Face In Frame Using Bounding Box
+
+            # TODO: Add Masks To Face On Original Image Using Facial Map
 
         # Show Video Frame
         cv.imshow("video", fr)
